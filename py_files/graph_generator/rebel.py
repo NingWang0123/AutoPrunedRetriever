0f6653e042f4e6f84252a9f40fb07187a3c8fb6e
Lance10t <100740200+Lancel0tz@users.noreply.github.com>
2025-09-21 05:20:12 +0800

    Add files via upload

--- py_files/graph_generator/rebel.py
+++ py_files/graph_generator/rebel.py
@@ -1,10 +1,5 @@
 from transformers import pipeline
 
-triplet_extractor = pipeline('text2text-generation', model='Babelscape/rebel-large', tokenizer='Babelscape/rebel-large')
-# We need to use the tokenizer manually since we need special tokens.
-extracted_text = triplet_extractor.tokenizer.batch_decode([triplet_extractor("Punta Cana is a resort town in the municipality of Higuey, in La Altagracia Province, the eastern most province of the Dominican Republic", return_tensors=True, return_text=False)[0]["generated_token_ids"]])
-print(extracted_text[0])
-# Function to parse the generated text and extract the triplets
 def extract_triplets(text):
     triplets = []
     relation, subject, relation, object_ = '', '', '', ''
