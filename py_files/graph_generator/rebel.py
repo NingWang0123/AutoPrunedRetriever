1a23a5ba83a5ac3b4fd4775ead2f74eb4ced4dd2
Lance10t <100740200+Lancel0tz@users.noreply.github.com>
2025-09-21 04:20:02 +0800

    Add files via upload

---
+++ py_files/graph_generator/rebel.py
@@ -0,0 +1,43 @@
+from transformers import pipeline
+
+triplet_extractor = pipeline('text2text-generation', model='Babelscape/rebel-large', tokenizer='Babelscape/rebel-large')
+# We need to use the tokenizer manually since we need special tokens.
+extracted_text = triplet_extractor.tokenizer.batch_decode([triplet_extractor("Punta Cana is a resort town in the municipality of Higuey, in La Altagracia Province, the eastern most province of the Dominican Republic", return_tensors=True, return_text=False)[0]["generated_token_ids"]])
+print(extracted_text[0])
+# Function to parse the generated text and extract the triplets
+def extract_triplets(text):
+    triplets = []
+    relation, subject, relation, object_ = '', '', '', ''
+    text = text.strip()
+    current = 'x'
+    for token in text.replace("<s>", "").replace("<pad>", "").replace("</s>", "").split():
+        if token == "<triplet>":
+            current = 't'
+            if relation != '':
+                triplets.append({'head': subject.strip(), 'type': relation.strip(),'tail': object_.strip()})
+                relation = ''
+            subject = ''
+        elif token == "<subj>":
+            current = 's'
+            if relation != '':
+                triplets.append({'head': subject.strip(), 'type': relation.strip(),'tail': object_.strip()})
+            object_ = ''
+        elif token == "<obj>":
+            current = 'o'
+            relation = ''
+        else:
+            if current == 't':
+                subject += ' ' + token
+            elif current == 's':
+                object_ += ' ' + token
+            elif current == 'o':
+                relation += ' ' + token
+    if subject != '' and relation != '' and object_ != '':
+        triplets.append({'head': subject.strip(), 'type': relation.strip(),'tail': object_.strip()})
+    triplets = {
+        (d['head'].strip(), d['type'].strip(), d['tail'].strip())
+        for d in triplets
+        if isinstance(d, dict) and d.get('head') and d.get('type') and d.get('tail')
+    }
+    return triplets
+
