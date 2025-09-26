fc23afd1e23f3d39ec06b4bed3560fa57c2d853f
NingWang0123 <1198137403wn@gmail.com>
2025-09-26 05:34:35 -0400

    Update pipeline_for_test.py

--- py_files/pipeline_for_test.py
+++ py_files/pipeline_for_test.py
@@ -40,7 +40,7 @@ TOPK_CTX     = 5
 # 1) Initialise embeddings & LLM
 # ---------------------------------------------------------------------
 
-def compress_rag_workflow(REPO_ID,CORPUS_FILE,QUEST_FILE,SEED_N,TEST_N,ini_meta_json = Path("meta_codebook.json") ,saved_examples_name = "pref_examples_medical.json",reward_func = None,reward_func_mode = 'non_llm',final_csv_path = "results/sbert_result"):
+def compress_rag_workflow(REPO_ID,CORPUS_FILE,QUEST_FILE,SEED_N,TEST_N,ini_meta_json = Path("meta_codebook.json") ,saved_examples_name = "sbert_pref_examples_medical.json",reward_func = None,reward_func_mode = 'non_llm',final_csv_path = "results/sbert_result"):
     print("» Initialising embeddings & LLM …")
     word_emb = Word2VecEmbeddings(model_name="word2vec-google-news-300")
     sent_emb = HuggingFaceEmbeddings(
