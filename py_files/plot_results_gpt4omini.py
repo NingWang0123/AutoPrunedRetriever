3e08f9b6a16956bf1a6a9b4436bec89ffe524775
NingWang0123 <1198137403wn@gmail.com>
2025-10-03 04:44:54 -0400

    Update plot_results_gpt4omini.py

--- py_files/plot_results_gpt4omini.py
+++ py_files/plot_results_gpt4omini.py
@@ -4,7 +4,22 @@ import numpy as np
 import json
 
 
-cr_metrics_path = 'results/generation_scores_apr.json'
+# cr_metrics_path = 'results/generation_scores_apr.json'
+
+# # load the json file
+
+# with open(cr_metrics_path, 'r', encoding='utf-8') as f:
+#     cr_metrics = json.load(f)
+
+
+# correctness = []
+
+
+# for dic in cr_metrics:
+#     correctness.append(dic['answer_correctness'])
+
+
+cr_metrics_path = 'meta_codebook.json'
 
 # load the json file
 
@@ -12,11 +27,7 @@ with open(cr_metrics_path, 'r', encoding='utf-8') as f:
     cr_metrics = json.load(f)
 
 
-correctness = []
-
-
-for dic in cr_metrics:
-    correctness.append(dic['answer_correctness'])
+print(len(cr_metrics['facts_lsts']))
 
 
 
