0032bb5887a9129954cf1637f76d4a9f0c2ef18b
NingWang0123 <1198137403wn@gmail.com>
2025-10-05 07:12:26 -0400

    Update pipeline_for_autopruned_openai_include_all.py

--- py_files/pipeline_for_autopruned_openai_include_all.py
+++ py_files/pipeline_for_autopruned_openai_include_all.py
@@ -412,7 +412,7 @@ if __name__ == "__main__":
 
     compress_rag_workflow(REPO_ID,CORPUS_FILE,QUEST_FILE,SEED_N,TEST_N, 
                             top_m,top_m*10,aft_combine_sim,aft_combine_sim,aft_combine_sim,0.95,
-                            Path("meta_codebook_new.json") ,f"pref_examples_medical_exact_openai_v3.json",reward_func,
+                            Path("meta_codebook.json") ,f"pref_examples_medical_exact_openai_v3.json",reward_func,
                             reward_func_mode = 'non_llm',final_json_path = f"results/compressrag_medical_data_openai_v3.json")
 
     # df.to_csv('results/result_sbertinclusive_new_embed_for_exactgraphrag.csv')
